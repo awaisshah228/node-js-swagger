fb587ff81e92733f7cbfeea836aa72fd94a9f449
awaisshah228 <m.awaisshah228.com>
2022-07-07 12:40:28 +0500

    deleted extra file

--- src/utils/swagger.js
+++
@@ -1,9 +0,0 @@
-const swaggerUI = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
-const options= require('./swaggerConfig')
-
-module.exports= async(app)=>{
-    const specs = swaggerJsDoc(options);
-    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-    
-}
